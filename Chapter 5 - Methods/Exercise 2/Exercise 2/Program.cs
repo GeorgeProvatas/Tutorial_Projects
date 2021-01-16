9d4852b1068d643bfb6bbb4f4faf3bfad5c5fac4
GeorgeProvatas <66174026+GeorgeProvatas@users.noreply.github.com>
2021-01-16 15:28:17 +0200

    Methods calling other methods

---
+++ Chapter 5 - Methods/Exercise 2/Exercise 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number");
+            int number = int.Parse(Console.ReadLine());
+            int squared = Square(number);
+            int cubed = Cube(number);
+            Console.WriteLine($"the square number is {squared} and the cube number is {cubed}");
+        }
+
+        public static int Square(int number)
+        {
+            int answer = number * number;
+            return answer;
+        }
+
+        public static int Cube(int number)
+        {
+            Square(number);
+            int answer = Square(number) * number;
+            return answer;
+        }
+    }
+}
