3beeb445bbf046350422749df9f5025a361b87d3
GeorgeProvatas <66174026+GeorgeProvatas@users.noreply.github.com>
2021-01-16 14:56:51 +0200

    Multiplication Method

--- Chapter 5 - Methods/Exercise 1/Exercise 1/Program.cs
+++ Chapter 5 - Methods/Exercise 1/Exercise 1/Program.cs
@@ -4,9 +4,19 @@ namespace Exercise_1
 {
     class Program
     {
+        public static void Multiplication_Table(int number)
+        {
+            for(int i = 2; i <= 10; i++)
+            {
+                int answer = number * i;
+                Console.WriteLine($"{number} * {i} = {answer}");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input a number");
+            int number = int.Parse(Console.ReadLine());
+            Multiplication_Table(number);
         }
     }
 }
